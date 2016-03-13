5a22e245cef6f9ad5d06fb36230affdb4c95f71b
suhussai <suhussai@ualberta.ca>
2016-03-13 14:36:06 -0600

    add stub methods for game_controller

---
+++ controller/game_controller.rb
@@ -0,0 +1,25 @@
+require "../model/board"
+require "../model/ai_player"
+require "../model/real_player"
+require "../model/game_piece"
+require "../model/game_pattern"
+
+class GameController
+
+    def initialize(board, *players)
+    end
+
+    def place_piece(piece, position)
+    end
+
+    def modify_player
+    end
+
+    def modify_game_piece
+    end
+
+    def modify_game_pattern
+    end
+
+end
+
