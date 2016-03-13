a7fe285f6f529affafdc3a7b22c4ffd4a6c1dfc5
suhussai <suhussai@ualberta.ca>
2016-03-13 14:51:11 -0600

    add stub methods for views: game_board_view and main_menu

---
+++ view/game_board_view.rb
@@ -0,0 +1,19 @@
+require "../controller/game_controller"
+
+def GameBoardView
+
+    def initialize()
+    end
+
+    def place_piece(piece, position)
+    end
+
+    def exit_game()
+    end
+
+    def print_board()
+        # did we decide to implement this?
+    end
+
+end
+
---
+++ view/main_menu.rb
@@ -0,0 +1,18 @@
+require "../controller/game_controller"
+
+def MainMenuView
+
+    def initialize()
+    end
+
+    def choose_mode(game_mode)
+    end
+
+    def new_game()
+    end
+
+    def exit_game()
+    end
+
+end
+
