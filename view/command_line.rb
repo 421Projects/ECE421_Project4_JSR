b7489d4fd3f20f434dba9ce9425cb25690d7814f
suhussai <suhussai@ualberta.ca>
2016-03-13 14:42:48 -0600

    add some stub methods to command_line view

---
+++ view/command_line.rb
@@ -0,0 +1,22 @@
+require "../controller/game_controller"
+
+def CommandLineView
+
+    def initialize()
+    end
+
+    def place_piece(piece, position)
+    end
+
+    def new_game()
+    end
+
+    def exit_game()
+    end
+
+    def print_board()
+        # did we decide to implement this?
+    end
+
+end
+
