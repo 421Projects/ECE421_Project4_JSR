30309e98091ee3dac311d19920c3c7cc0b44ba32
suhussai <suhussai@ualberta.ca>
2016-03-14 15:56:33 -0600

    add pre/post conditions for board

--- model/board.rb
+++ model/board.rb
@@ -1,23 +1,38 @@
 require "./game_piece"
 require "./game_pattern"
+require 'contracts'
 
 class Board
 
+    include Contracts::Core
+    include Contracts::Builtin
+    include Contracts::Invariants
+
+    Contract Contracts::Nat,Contracts::Nat => Any
 	def initialize(width, height)
 		@width = width
 		@height = height
 		@board = Hash.new
 	end
 
+    Contract ArrayOf[GamePattern] => Bool
 	def analyze(pattern_array)
 		#Looks for all the given patterns in the board
+        return false
 	end
 
+    Contract Contracts::Nat,Contracts::Nat, GamePiece => None
 	def set_piece(x, y, piece)
 		@board[[x,y]] = piece
 	end
 
+    Contract Contracts::Nat,Contracts::Nat => GamePiece
 	def get_piece(x, y)
 		return @board[[x, y]]
 	end
+
+    Contract None => Pos
+    def get_piece_count()
+        return 0
+    end
 end
