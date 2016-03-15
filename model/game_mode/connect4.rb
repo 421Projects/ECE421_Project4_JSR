213a9fd0f0013b8f45bcf6f1c599580b46788dec
Jared Rewerts <rewerts@ualberta.ca>
2016-03-15 14:24:28 -0600

    Add invariants and constraints to the game modes.

--- model/game_mode/connect4.rb
+++ model/game_mode/connect4.rb
@@ -4,39 +4,46 @@ require_relative "../game_pieces/red"
 
 class Connect4 < GameMode
 
+    invariant(@num_players) {@num_players == @og_num_players}
+    invariant(@player_pieces) {@player_pieces == @og_player_pieces}
+    invariant(@player_patterns) {@player_patterns == @og_player_patterns}
+
     def initialize()
         @num_players = 2
+        @og_num_players = @num_players
         @player_pieces = []
         @player_patterns = []
 
         @player_pieces.push(BlackPiece.new)
         @player_pieces.push(RedPiece.new)
 
+        @og_player_pieces = @player_pieces
+
         # Create patterns for Black Pieces
 
         black_patterns = []
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[0, 0]] = BlackPiece.new
         pattern[[0, 1]] = BlackPiece.new
         pattern[[0, 2]] = BlackPiece.new
         pattern[[0, 3]] = BlackPiece.new
         black_patterns << pattern
 
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[0, 0]] = BlackPiece.new
         pattern[[1, 0]] = BlackPiece.new
         pattern[[2, 0]] = BlackPiece.new
         pattern[[3, 0]] = BlackPiece.new
         black_patterns << pattern
 
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[0, 0]] = BlackPiece.new
         pattern[[1, 1]] = BlackPiece.new
         pattern[[2, 2]] = BlackPiece.new
         pattern[[3, 3]] = BlackPiece.new
         black_patterns << pattern
 
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[3, 0]] = BlackPiece.new
         pattern[[2, 1]] = BlackPiece.new
         pattern[[1, 2]] = BlackPiece.new
@@ -47,34 +54,36 @@ class Connect4 < GameMode
 
 
         red_patterns = []
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[0, 0]] = RedPiece.new
         pattern[[0, 1]] = RedPiece.new
         pattern[[0, 2]] = RedPiece.new
         pattern[[0, 3]] = RedPiece.new
         red_patterns << pattern
 
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[0, 0]] = RedPiece.new
         pattern[[1, 0]] = RedPiece.new
         pattern[[2, 0]] = RedPiece.new
         pattern[[3, 0]] = RedPiece.new
         red_patterns << pattern
 
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[0, 0]] = RedPiece.new
         pattern[[1, 1]] = RedPiece.new
         pattern[[2, 2]] = RedPiece.new
         pattern[[3, 3]] = RedPiece.new
         red_patterns << pattern
 
-        pattern = Hash.new
+        pattern = GamePattern.new
         pattern[[3, 0]] = RedPiece.new
         pattern[[2, 1]] = RedPiece.new
         pattern[[1, 2]] = RedPiece.new
         pattern[[0, 3]] = RedPiece.new
         red_patterns << pattern
         
-        player_patterns << red_patterns
+        @player_patterns << red_patterns
+
+        @og_player_patterns = @player_patterns
     end
 end
--- model/game_mode/otto_toot.rb
+++ model/game_mode/otto_toot.rb
@@ -1,5 +1,8 @@
 require_relative "./game_mode"
 
 class OttoToot < GameMode
-
+    invariant(@num_players) {@num_players == @og_num_players}
+    invariant(@player_pieces) {@player_pieces == @og_player_pieces}
+    invariant(@player_patterns) {@player_patterns == @og_player_patterns}
+    
 end
