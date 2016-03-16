21a74720d499a76b809e22bc0ede88eef7d86463
suhussai <suhussai@ualberta.ca>
2016-03-15 19:02:07 -0600

    add more tests

--- model/model_tests.rb
+++ model/model_tests.rb
@@ -132,14 +132,44 @@ class Connect4ModelTest < Test::Unit::TestCase
         # place off of board (in y direction, in x direction...)
         # dont include piece
         # dont include coords
-        b = Board.new(6,7)
+        b = Board.new(1,1)
 
         b.set_piece(1, BlackPiece.new)
         assert(b.get_piece(1,1).is_a?(GamePiece),
-                                      "asd")
-        assert(b.get_piece(1,1).is_a? BlackPiece)
+               "Piece at position (1,1) is not a GamePiece.")
+        assert(b.get_piece(1,1).is_a?(BlackPiece),
+              "Piece at position (1,1) is not a BlackPiece.")
+
+        assert_raise ColumnFullError do
+            b.set_piece(1, BlackPiece.new)
+        end
+        assert_raise ColumnFullError do
+            b.set_piece(1, RedPiece.new)
+        end
+        assert(b.get_piece(1,1).is_a?(GamePiece),
+               "Piece at position (1,1) is not a GamePiece.")
+        assert(b.get_piece(1,1).is_a?(BlackPiece),
+              "Piece at position (1,1) is not a BlackPiece.")
+
+        assert_raise ColumnExceedsBoardWidthError do
+            b.set_piece(2341, BlackPiece.new)
+        end
+
+        assert_raise do
+            b.set_piece(1)
+        end
+
+        assert_raise do
+            b.set_piece(BlackPiece.new)
+        end
+
+        assert_raise do
+            b.set_piece(RedPiece.new)
+        end
+
         assert_equal(b.analyze, false,
                      "Wrongly calculated the game to be won.")
     end
 
 end
+
