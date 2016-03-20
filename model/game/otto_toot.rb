80ee5176d78bd163d9b5305db152a3f636a0787d
Jared Rewerts <rewerts@ualberta.ca>
2016-03-19 21:13:09 -0600

    Filled out the constructor for Otto/Toot.

--- model/game/otto_toot.rb
+++ model/game/otto_toot.rb
@@ -1,8 +1,85 @@
 require_relative "./game"
 
 class OttoToot < GameMode
-    invariant(@num_players) {@num_players == @og_num_players}
-    invariant(@player_pieces) {@player_pieces == @og_player_pieces}
-    invariant(@player_patterns) {@player_patterns == @og_player_patterns}
+
+    def initialize()
+        @p1_piece = "O"
+        @p2_piece = "T"
+
+        @board_width = 7
+        @board_height = 6
+
+        # This will hold the images related to each piece:
+        # String => Image
+
+        # @patterns = {
+        #     p1 => [
+        #         {
+
+        #         }
+        #     ]
+        # }
+
+        # Create patterns for Black Pieces
+
+        @p1_patterns = []
+        pattern = {}
+        pattern[[0, 0]] = @p1_piece
+        pattern[[0, 1]] = @p2_piece
+        pattern[[0, 2]] = @p2_piece
+        pattern[[0, 3]] = @p1_piece
+        @p1_patterns << pattern
+
+        pattern = {}
+        pattern[[0, 0]] = @p1_piece
+        pattern[[1, 0]] = @p2_piece
+        pattern[[2, 0]] = @p2_piece
+        pattern[[3, 0]] = @p1_piece
+        @p1_patterns << pattern
+
+        pattern = {}
+        pattern[[0, 0]] = @p1_piece
+        pattern[[1, 1]] = @p2_piece
+        pattern[[2, 2]] = @p2_piece
+        pattern[[3, 3]] = @p1_piece
+        @p1_patterns << pattern
+
+        pattern = {}
+        pattern[[3, 0]] = @p1_piece
+        pattern[[2, 1]] = @p2_piece
+        pattern[[1, 2]] = @p2_piece
+        pattern[[0, 3]] = @p1_piece
+        @p1_patterns << pattern
+
+        @p2_patterns = []
+        pattern = {}
+        pattern[[0, 0]] = @p2_piece
+        pattern[[0, 1]] = @p1_piece
+        pattern[[0, 2]] = @p1_piece
+        pattern[[0, 3]] = @p2_piece
+        @p2_patterns << pattern
+
+        pattern = {}
+        pattern[[0, 0]] = @p2_piece
+        pattern[[1, 0]] = @p1_piece
+        pattern[[2, 0]] = @p1_piece
+        pattern[[3, 0]] = @p2_piece
+        @p2_patterns << pattern
+
+        pattern = {}
+        pattern[[0, 0]] = @p2_piece
+        pattern[[1, 1]] = @p1_piece
+        pattern[[2, 2]] = @p1_piece
+        pattern[[3, 3]] = @p2_piece
+        @p2_patterns << pattern
+
+        pattern = {}
+        pattern[[3, 0]] = @p2_piece
+        pattern[[2, 1]] = @p1_piece
+        pattern[[1, 2]] = @p1_piece
+        pattern[[0, 3]] = @p2_piece
+        @p2_patterns << pattern
+
+    end
     
 end
